b08562d229166da2fe268069f23aa782bdb41be9
oscar310394 <oscar310394@gmail.com>
2018-02-23 16:30:23 -0600

    Project REST Lumen

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,19 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
+
+$router->post('/users/login', ['uses' => 'UsersController@getToken']);
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->get('/key', function () {
+    return str_random(32);
+});
+
+$router->group(['middleware' => ['auth']], function() use ($router) {
+    $router->get('/users', ['uses' => 'UsersController@index']);
+    $router->post('/users', ['uses' => 'UsersController@createUser']);
 });
